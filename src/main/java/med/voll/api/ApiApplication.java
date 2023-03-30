a1e1cc9861a5aaa6cfe5e80c0b7487c151efc8f1
mtheusbrito <matheusbritodasilva@gmail.com>
2023-03-30 01:20:41 -0300

    build com arquivo .war

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -2,9 +2,23 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+// Para permitir o deploy em tomcat
+// a classe ApiApplication deve extender a classe SpringBootServletInitializer e o metodo configure deve ser sobreescrito
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication  extends SpringBootServletInitializer{
+	
+	
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(ApiApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
